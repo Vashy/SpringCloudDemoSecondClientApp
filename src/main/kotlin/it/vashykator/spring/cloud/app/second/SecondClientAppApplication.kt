99cc2035c811edf0f600ac32d743538e51d08eaf
Timoty Granziero <timoty.granziero@arcavita.it>
2021-06-09 09:27:05 +0200

    Initial configurations

--- src/main/kotlin/it/vashykator/spring/cloud/app/second/SecondClientAppApplication.kt
+++ src/main/kotlin/it/vashykator/spring/cloud/app/second/SecondClientAppApplication.kt
@@ -2,10 +2,32 @@ package it.vashykator.spring.cloud.app.second
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.ws.rs.GET
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 class SecondClientAppApplication
 
 fun main(args: Array<String>) {
 	runApplication<SecondClientAppApplication>(*args)
 }
+
+@RestController
+class Caller(private val firstAppClient: FirstAppClient) {
+	@GetMapping("/example")
+	fun example() = firstAppClient.myEndpoint()
+}
+
+@FeignClient("first-client-app")
+interface FirstAppClient {
+	@GetMapping("/my-endpoint")
+	fun myEndpoint(): Par
+}
+
+data class Par(val first: String, val second: String)
